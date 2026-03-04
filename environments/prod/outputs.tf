8b5f106f8efd5a90b1a81d6a598a4c00287674e2
Tommy Tang <ttangwork@gmail.com>
2026-03-04 16:49:57 +1100

    fix(terraform): add try to test outputs conditionally

--- environments/prod/outputs.tf
+++ environments/prod/outputs.tf
@@ -1,13 +1,13 @@
 output "cluster_name" {
-  value = module.gke[0].cluster_name
+  value = try(module.gke[0].cluster_name, "")
 }
 
 output "cluster_endpoint" {
-  value     = module.gke[0].cluster_endpoint
+  value     = try(module.gke[0].cluster_endpoint, "")
   sensitive = true
 }
 
 output "get_credentials_command" {
   description = "Command to get kubeconfig (run from bastion)"
-  value       = "gcloud container clusters get-credentials ${module.gke[0].cluster_name} --region=${var.region} --project=${var.project_id} --internal-ip"
+  value       = try("gcloud container clusters get-credentials ${module.gke[0].cluster_name} --region=${var.region} --project=${var.project_id} --internal-ip", "")
 }

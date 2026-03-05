1d3b6a7df39050da8d210cfa4570c831dc84e843
Tommy Tang <ttangwork@gmail.com>
2026-03-05 18:39:41 +1100

    fix(deploy scripts): updated deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,18 +8,14 @@ kubectl create namespace "$NAMESPACE" --dry-run=client -o yaml | kubectl apply -
 
 # deploy book-info app with or without HPA based on the flag
 if [ "$HPA_ENABLED" = true ]; then
-  echo "Deploying HPA for book-info"
-  # this yaml file has resource requests/limits for HPA to work
-  kubectl apply -n "$NAMESPACE" -f ./bookinfo-app/deploy.yaml
-  kubectl apply -n "$NAMESPACE" -f ./configs/hpa.yaml
+  echo "Deploying book-info with HPA enabled"
+  kubectl apply -n "$NAMESPACE" -f ./bookinfo-app
+  kubectl apply -n "$NAMESPACE" -f ./configs
 else
   echo "Deploying book-info without HPA"
   # this yaml file doesn't define resource requests/limits so HPA won't work
   kubectl apply -n "$NAMESPACE" -f https://raw.githubusercontent.com/istio/istio/release-1.29/samples/bookinfo/platform/kube/bookinfo.yaml
 fi
 
-# deploy gateway
-kubectl apply -f ./gateway-api/gateway.yaml
-
-# deploy HTTPRoute
-kubectl apply -f ./gateway-api/http-route.yaml
+# deploy gateway api resources
+kubectl apply -f ./gateway-api
